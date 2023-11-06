62996201a2950d592a8ffc884e1a85729c9cb8c8
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2023-11-06 09:48:58 +0100

    ADD commande remplacer dans CommandeFactory.class

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -1,2 +1,25 @@
-package fr.iut.editeur.commande;public class CommandeRemplacer {
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        String texte = parameters[3];
+        if (parameters[3] == null) {
+            this.document.remplacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), "");
+        } else {
+            this.document.remplacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), parameters[3]);
+        }
+        super.executer();
+    }
 }
