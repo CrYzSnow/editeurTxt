aef6bf620a124e6475e4cae0e77b2423b17e7a49
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2023-11-09 10:58:29 +0100

    feat: ajout fonctionalité effacer
    feat: ajout feature effacer

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -32,6 +32,10 @@ public class Document {
         remplacer(start,end,part);
     }
 
+    public void effacer(int start, int end) {
+        remplacer(start, end, "");
+    }
+
     @Override
     public String toString() {
         return this.texte;
